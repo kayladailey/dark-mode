3a977e70701c53f8b88b0f0b601a6e2ac98d3608
Kayla Dailey <kayladailey000@gmail.com>
2020-01-23 21:03:51 -0600

    completed step 2  useDarkMode

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -8,12 +8,11 @@ const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);
     // Parse and return stored json or, if undefined, return initialValue
     return item ? JSON.parse(item) : initialValue;
-
-
-    const setValue = value => {
-        setStoredValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value));
-    }
   });
+
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
 
 return [storedValue];
